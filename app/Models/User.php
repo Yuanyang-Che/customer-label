819246871c4f37746b9129555e812bc1b576692a
Kevin Che <cheyuany@usc.edu>
2022-05-05 14:57:40 -0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,7 +51,7 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Role::class);
     }
-    
+
     public function labels()
     {
         return $this->hasMany(Label::class);
